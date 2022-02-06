d465a2ead2e9f6658387c5f6e19c1c1152b680a4
Praveen Kumar <praveenscience@gmail.com>
2022-02-06 19:51:26 +0530

    Converted App into a Class Component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
+import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header className="Header justify-content-center" dark={true}>
-        Tweetinator
-      </Header>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header className="Header justify-content-center" dark={true}>
+          Tweetinator
+        </Header>
+      </div>
+    );
+  }
+}
 
 export default App;
